937e3d7f96e23903c9b1b985cd3012f497fea8e8
li ya <1423377245@qq.com>
2019-03-13 08:47:57 +0800

    仿真AC-2017globalcom

--- user_environment.py
+++ user_environment.py
@@ -13,28 +13,28 @@ class envr(object):
         a1=a[0];a2=a[1]
         if a1+a2<=s[2]:
             ratio=B*(math.log2(1+H[0] * a1/σ)+math.log2(1+H[1] * a2/σ))/1
-            return np.array([1+H[0] * a1/σ,1+H[1] * a2/σ,np.random.uniform(0,1)]),ratio
-            #return np.array([1 + H[0] * a1 / σ,1 + H[1] * a2 / σ,0.7]), ratio
+            #return np.array([1+H[0] * a1/σ,1+H[1] * a2/σ,np.random.uniform(0,1)]),ratio
+            return np.array([1 + H[0] * a1 / σ,1 + H[1] * a2 / σ,0.7]), ratio
         else:
             if a1<=s[2] and a2<=s[2]:
                 ratio1 = B * (math.log2(1 + H[0] * a1 / σ) + math.log2(1 + H[1] * a2 / σ)) /(1+a2)
                 ratio2 = B * (math.log2(1 + H[0] * a1 / σ) + math.log2(1 + H[1] * a2 / σ)) / (1+ a1)
                 ratio_tol=[ratio1,ratio2]
                 index=ratio_tol.index(max(ratio_tol))
-                return np.array([1+H[0] * a1/σ,1+H[1] * a2/σ,np.random.uniform(0,1)]),ratio_tol[index]
-                #return np.array([1 + H[0] * a1 / σ,1 + H[1] * a2 / σ,0.7]), ratio_tol[index]
+                #return np.array([1+H[0] * a1/σ,1+H[1] * a2/σ,np.random.uniform(0,1)]),ratio_tol[index]
+                return np.array([1 + H[0] * a1 / σ,1 + H[1] * a2 / σ,0.7]), ratio_tol[index]
             elif a1>=s[2] and a2<=s[2]:
                 ratio = B * (math.log2(1 + H[0] * a1 / σ) + math.log2(1 + H[1] * a2 / σ)) / (1 + a1)
-                return np.array([1 + H[0] * a1 / σ, 1 + H[1] * a2 / σ, np.random.uniform(0, 1)]), ratio
-                #return np.array([1 + H[0] * a1 / σ,1 + H[1] * a2 / σ,0.7]), ratio
+                #return np.array([1 + H[0] * a1 / σ, 1 + H[1] * a2 / σ, np.random.uniform(0, 1)]), ratio
+                return np.array([1 + H[0] * a1 / σ,1 + H[1] * a2 / σ,0.7]), ratio
             elif a1<=s[2] and a2>=s[2]:
                 ratio = B * (math.log2(1 + H[0] * a1 / σ) + math.log2(1 + H[1] * a2 / σ)) / (1+ a2)
                 return np.array([1 + H[0] * a1 / σ, 1 + H[1] * a2 / σ, np.random.uniform(0, 1)]), ratio
                 #return np.array([1 + H[0] * a1 / σ,1 + H[1] * a2 / σ,0.7]), ratio
             elif a1>=s[2] and a2>=s[2]:
                 ratio = B * (math.log2(1 + H[0] * a1 / σ) + math.log2(1 + H[1] * a2 / σ)) / (1 + a2+a1)
-                return np.array([1 + H[0] * a1 / σ, 1 + H[1] * a2 / σ, np.random.uniform(0, 1)]), ratio
-                #return np.array([1 + H[0] * a1 / σ,1 + H[1] * a2 / σ,0.7]), ratio
+                #return np.array([1 + H[0] * a1 / σ, 1 + H[1] * a2 / σ, np.random.uniform(0, 1)]), ratio
+                return np.array([1 + H[0] * a1 / σ,1 + H[1] * a2 / σ,0.7]), ratio
 
 
 
